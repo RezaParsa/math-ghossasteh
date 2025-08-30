5b1fc771a2c6aac2412ba2b5df48b26256469d5b
Reza <rparsa75@gmail.com>
2025-08-30 22:36:19 +0330

    UPGRADE : 4th code , like others now have some input to take

--- 4/4/_4.py
+++ 4/4/_4.py
@@ -1,26 +1,69 @@
 ﻿
-# ماتریس بولی اول ما
-a = [
-  [1, 0, 1],
-  [0, 1, 0]
-]
 
-# ماتریس بولی دوم ما
-b = [
-  [0, 1],
-  [1, 0],
-  [0, 1]
-]
+while True:
+    try:
+        n_a = int(input("pls enter matrix A size: "))
+        break
+    except ValueError:
+        print("error : matrix size must be posetive num.")
+
+matrix_a = []
+print(f"pls {n_a} row of num must be enter with space:")
+
+for i in range(n_a):
+    while True:
+        try:
+            row_str = input(f"row {i+1}: ")
+            row = [int(x) for x in row_str.split()]
+            if len(row) == n_a:
+                matrix_a.append(row)
+                break
+            else:
+                print(f"error: count of this row ({len(row)}) wiht size of matrix  ({n_a}) is not suit pls renter it .")
+        except ValueError:
+            print("error enter nums with space .")
+
+print("first matrix done")
+
+# --- دریافت ماتریس دوم (B) ---
+print("\n--------------------------")
+
+while True:
+    try:
+        n_b = int(input("pls enter matrix B size: "))
+        break
+    except ValueError:
+        print("erroe matrix size must be in podetive nums.")
+
+matrix_b = []
+print(f"pls {n_b} row of nub must enter with space:")
+
+for i in range(n_b):
+    while True:
+        try:
+            row_str = input(f"row {i+1}: ")
+            row = [int(x) for x in row_str.split()]
+            if len(row) == n_b:
+                matrix_b.append(row)
+                break
+            else:
+                print(f"error: count of this row  ({len(row)}) with matrix size ({n_b}) is not suit pls enter it again.")
+        except ValueError:
+            print("error: pls enter num just with space.")
+
+print("secndry matrix done.")
+
+
 
 # پیدا کردن ابعاد ماتریس‌ها
-rows_a = len(a)
-cols_a = len(a[0])
-rows_b = len(b)
-cols_b = len(b[0])
+rows_a = len(matrix_a)
+cols_a = len(matrix_a[0])
+rows_b = len(matrix_b)
+cols_b = len(matrix_b[0])
 
 # بررسی اینکه آیا ضرب بولی امکان‌پذیر است؟
 if cols_a == rows_b:
-  print("zarbboly in possible.")
+  print("zarbboly is possible.")
   
   # یک جدول خالی برای نتیجه ضرب می‌سازیم
   result = []
@@ -36,7 +79,7 @@ if cols_a == rows_b:
         # این جا جادوی ضرب بولی اتفاق می‌افتد
         # a[x][z] AND b[z][y]
         # سپس با OR جمع می‌کنیم
-        cell_value = cell_value or (a[x][z] and b[z][y])
+        cell_value = cell_value or (matrix_a[x][z] and matrix_b[z][y])
       
       new_row.append(cell_value)
     
